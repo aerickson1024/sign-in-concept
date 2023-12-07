31737cb01c44082591f688357b561c61ab19e9f8
andy erickson <andy.w.erickson@gmail.com>
2023-12-06 19:46:47 -0600

    good start on login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 export const routes: Routes = [{
   path: '',
+  component: SignInComponent
+}, {
+  path: 'home',
   component: HomeComponent
 }];
