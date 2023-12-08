214af24eda0796c12ac8ba63ea4e118d87fe5452
andy erickson <andy.w.erickson@gmail.com>
2023-12-07 22:37:18 -0600

    sign up page look and validation is done

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,13 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ReactiveFormsModule, FormControl, FormGroup, Validators, ValidationErrors, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.less'
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit {
+  profileForm: FormGroup;
 
+  constructor() {
+    this.profileForm = new FormGroup({
+      'email': new FormControl('', [Validators.required, Validators.email]),
+      'password': new FormControl('', Validators.compose([Validators.required, Validators.minLength(4)])),
+      'passwordAgain': new FormControl('', Validators.compose([Validators.required]))
+    }, this.passwordValidator());
+  }
+
+  ngOnInit(): void {}
+
+  handleSubmit() {
+    console.log('FORM SUBMITTED');
+    console.log(`Email: ${this.profileForm.value.email}`);
+    console.log(`Password: ${this.profileForm.value.password}`);
+    console.log(`Password Again: ${this.profileForm.value.passwordAgain}`);
+  }
+
+  passwordValidator() {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const password = this.profileForm?.value.password;
+      const passwordAgain = this.profileForm?.value.passwordAgain;
+
+      console.log(`P1: ${password}`);
+      console.log(`P2: ${passwordAgain}`);
+
+      const areNotEqual = password != passwordAgain;
+      return areNotEqual ? { areNotEqual: true } : null;
+    }
+  }
 }
